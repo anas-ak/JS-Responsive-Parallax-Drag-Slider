583660a6ec9e09251f5f185113b950a36e0f7731
Anas <71920781+anas-ak@users.noreply.github.com>
2021-05-25 13:11:25 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ $(document).ready(function() {
     let diff = 0;
 
     // Generating slides
-    let arrCities = ['Amsterdam', 'Rome', 'New-York', 'Singapore', 'Prague'] // Change no. of slides in CSS also
+    let arrCities = ['Amsterdam', 'Rome', 'New—York', 'Singapore', 'Prague']; // Change no. of slides in CSS also
     let numOfCities = arrCities.length;
     let arrCitiesDivided = [];
 
     arrCities.map(city => {
         let length = city.length;
         let letters = Math.floor(length / 4);
-        let exp = new RegExp(".{1, " + letters + "}", "g");
+        let exp = new RegExp(".{1," + letters + "}", "g");
 
         arrCitiesDivided.push(city.match(exp));
     });
@@ -40,18 +40,15 @@ $(document).ready(function() {
                 ${firstLetter}
            </div>`);
 
-        for(let i = 0, length = arrCitiesDivided[city].length; i < length; i++) {
-            if(window.CP.shouldStopExecution(0)) break;
-
+        for (let i = 0, length = arrCitiesDivided[city].length; i < length; i++) {
             const text = 
-                $(`<div class="slide__text slide__text--%{i + 1}">
+                $(`<div class="slide__text slide__text--${i + 1}">
                         ${arrCitiesDivided[city][i]}
                    </div>`);
                 frag1.append(text);
-        }window.CP.exitedLoop(0);
+        }
 
-        const navSlide = $(`<li data-target="${numSlide}" class="nav__slide nav__slide--${numSlide}">
-                            </li>`);
+        const navSlide = $(`<li data-target="${numSlide}" class="nav__slide nav__slide--${numSlide}"></li>`);
         frag2.append(navSlide);
         $nav.append(frag2);
 
@@ -64,10 +61,8 @@ $(document).ready(function() {
     };
 
     for(let i = 0, length = numOfCities; i < length; i++){
-        if(window.CP.shouldStopExecution(1)) break;
-
         generateSlide(i);
-    }window.CP.exitedLoop(1);
+    }
 
     $('.nav__slide--1').addClass('nav-active');
 
@@ -86,16 +81,15 @@ $(document).ready(function() {
         diff = 0;
         $slider.addClass('animation');
         $slider.css({
-            'transform': 'translate3d(-' + (curSlide - direction) * 100 + '%, 0, 0)'
-        });
+            'transform': 'translate3d(-' + (curSlide - direction) * 100 + '%, 0, 0)'});
 
         $slider.find('.slide__darkbg').css({
-            'transform' : 'translate3d(' + (curSlide - direction) * 50 + '%, 0, 0)'
-        });
+            'transform': 'translate3d(' + (curSlide - direction) * 50 + '%, 0, 0)'});
 
+        $slider.find('.slide__letter').css({
+                'transform': 'translate3d(0, 0, 0)' });
         $slider.find('.slide__text').css({
-            'transform' : 'translate3d(0, 0, 0)'
-        });
+            'transform': 'translate3d(0, 0, 0)' });
     }
 
     function navigateRight() {
@@ -108,5 +102,86 @@ $(document).ready(function() {
         bullets(curSlide + 1);
         curSlide++;
     }
-    /********************** */
-})
+
+    function navigateLeft() {
+        if(curSlide <= 1) return;
+        pagination(2);
+        setTimeout(timeout, animSpd);
+        bullets(curSlide - 1);
+        curSlide--;
+    }
+
+    function toDefault() {
+        pagination(1);
+        setTimeout(timeout, animSpd);
+    }
+
+    // Events
+    $(document).on('mousedown touchstart', '.slide', function(e) {
+        if(animation) return;
+
+        let target = +$(this).attr('data-target');
+        let startX = e.pageX || e.originalEvent.touches[0].pageX;
+        $slider.removeClass('animation');
+
+        $(document).on('mousemove touchmove', function(e) {
+            let x = e.pageX || e.originalEvent.touches[0].pageX;
+            diff = startX - x;
+            if(target === 1 && diff < 0 || target == numOfCities && diff > 0) return;
+
+            $slider.css({
+                'transform': 'translate3d(-' + ((curSlide - 1) * 100 + diff / 30) + '%, 0, 0)' });
+
+            $slider.find('.slide__darkbg').css({
+                  'transform': 'translate3d(' + ((curSlide - 1) * 50 + diff / 60) + '%, 0, 0)' });
+          
+            $slider.find('.slide__letter').css({
+                  'transform': 'translate3d(' + diff / 60 + 'vw, 0, 0)' });
+          
+            $slider.find('.slide__text').css({
+                  'transform': 'translate3d(' + diff / 15 + 'px, 0, 0)' });
+        }); 
+    });
+
+    $(document).on('mouseup touchend', function(e) {
+        $(document).off('mousemove touchmove');
+
+        if(animation) return;
+
+        if(diff >= distOfLetGo) {
+            navigateRight();
+        } else if (diff <= -distOfLetGo) {
+            navigateLeft();
+        } else {
+            toDefault();
+        }
+    });
+
+    $(document).on('click', '.nav__slide:not(.nav-active)', function(){
+        let target = +$(this).attr('data-target');
+        bullets(target);
+        curSlide = target;
+        pagination(1);
+    });
+
+    $(document).on('click', '.side-nav', function() {
+        let target = $(this).attr('data-target');
+        
+        if(target === 'right') navigateRight();
+        if(target === 'left') navigateLeft();
+    });
+
+    $(document).on('keydown', function(e) {
+        if(e.which === 39) navigateRight();
+        if(e.which === 37) navigateLeft();
+    });
+
+    $(document).on('mousewheel DOMMouseScroll', function(e) {
+        if(animation) return;
+
+        let delta = e.originalEvent.wheelDelta;
+
+        if(delta > 0 || e.originalEvent.detail < 0) navigateLeft();
+        if(delta < 0 || e.originalEvent.detail > 0) navigateRight();
+    });
+});
